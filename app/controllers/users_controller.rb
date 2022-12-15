ecb78ee521b4aa82e66359fabb58fa64a7ba9a66
Austin Longo <austinlongocontracting@gmail.com>
2022-12-15 16:57:12 -0500

    commit tree da835ab9767e1c77ce2c2b18751057d0418612ff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+    def show 
+        user = User.find_by(id: params[:id])
+        render json: user, include: :items
+    end
+
+    # def index
+    #     users = Users.all
+    #     render json: users, include: :item
+    # end
 end
